1a80c7b926fdb00427fb6d8b0650d137ab92488b
Chris Lerus <lerus.chris@gmail.com>
2015-01-22 16:41:50 -0500

    player in progress

--- assets/js/content_manager/base.js
+++ assets/js/content_manager/base.js
@@ -1,71 +1,82 @@
 function uploadFile()
 {
     $('#fileupload').fileupload({
-        dataType: 'json',
-        add: function (e, data) {
-            data.context = $('<button/>').text('Upload')
-                .appendTo(document.body)
-                .click(function () {
-                    data.context = $('<p/>').text('Uploading...').replaceAll($(this));
-                    data.submit();
-                });
-        },
-        done: function (e, data) {
-            data.context.text('Upload finished.');
-        }
-    });
+                                    dataType: 'json',
+                                    add: function (e, data) {
+                                        data.context = $('<button/>').text('Upload')
+                                            .appendTo(document.body)
+                                            .click(function () {
+                                                       data.context = $('<p/>').text('Uploading...').replaceAll($(this));
+                                                       data.submit();
+                                                   });
+                                    },
+                                    done: function (e, data) {
+                                        data.context.text('Upload finished.');
+                                    }
+                                });
 }
 
 function log_in()
 {
     $.ajax({
-        type : "POST",
-        data : {
-            username: $("#username").val(),
-            password: $("#password").val()
-        },
-        url : "/manager/login/",
-        error: function(newData) {
-            $("#errors").html(newData);
-        },
-        complete: function(status, text){
-            $("#errors").html(text);
-        },
-        success: function() {
-            window.location.replace("http://127.0.0.1:8000/manager/");
-        }
-    });
+               type : "POST",
+               data : {
+                   username: $("#username").val(),
+                   password: $("#password").val()
+               },
+               url : "/manager/login/",
+               error: function(newData) {
+                   $("#errors").html(newData);
+               },
+               complete: function(status, text){
+                   $("#errors").html(text);
+               },
+               success: function() {
+                   window.location.replace("http://127.0.0.1:8000/manager/");
+               }
+           });
 }
 
 $(function () {
     $('#fileupload').fileupload({
-        dataType: 'json',
-        done: function (e, data) {
-            $.each(data.result.files, function (index, file) {
-                $('<p/>').text(file.name).appendTo(document.body);
-            });
-        },
-        progressall: function (e, data) {
-            var progress = parseInt(data.loaded / data.total * 100, 10);
-            $('#progress .bar').css(
-                'width',
-                    progress + '%'
-            );
-        },
-        add: function (e, data) {
-            data.context = $('<button/>').text('Upload')
-                .appendTo(document.body)
-                .click(function () {
-                    data.context = $('<p/>').text('Uploading...').replaceAll($(this));
-                    data.submit();
-                });
-        }
-    });
+                                    dataType: 'json',
+                                    done: function (e, data) {
+                                        $.each(data.result.files, function (index, file) {
+                                            $('<p/>').text(file.name).appendTo(document.body);
+                                        });
+                                    },
+                                    progressall: function (e, data) {
+                                        var progress = parseInt(data.loaded / data.total * 100, 10);
+                                        $('#progress .bar').css(
+                                            'width',
+                                            progress + '%'
+                                        );
+                                    },
+                                    add: function (e, data) {
+                                        data.context = $('<button/>').text('Upload')
+                                            .appendTo(document.body)
+                                            .click(function () {
+                                                       data.context = $('<p/>').text('Uploading...').replaceAll($(this));
+                                                       data.submit();
+                                                   });
+                                    }
+                                });
 });
+
+function toggle_core_animated(id)
+{
+    var p = document.querySelector('#' +  id);
+    p.selected = p.selected ? 0 : 1;
+}
 
 function stuff(x) {
     var p = document.querySelector('#main_layer');
     p.selected = x;
+}
+
+function toggle_left_column() {
+    var p = document.querySelector('#profile_layer');
+    p.selected = p.selected ? 0 : 1;
 }
 
 function to_main_to_user(id, x)
@@ -73,6 +84,20 @@ function to_main_to_user(id, x)
     $(".prof_pix_heroable[hero-id='profile-pix-hero']").attr("hero-id", "");
     $('#' + id).attr('hero-id', 'profile-pix-hero');
     var p = document.querySelector('#main_layer');
+    p.selected = x;
+}
+
+function to_user_to_library(id, x)
+{
+    $(".lib_pix_heroable[hero-id='lib-pix-hero']").attr("hero-id", "");
+    $('#' + id).attr('hero-id', 'lib-pix-hero');
+    var p = document.querySelector('#explore_user_libraries');
+    p.selected = x;
+}
+
+function to_library_to_user(x)
+{
+    var p = document.querySelector('#explore_user_libraries');
     p.selected = x;
 }
 
@@ -103,6 +128,24 @@ function tog_dialog() {
 }
 
 function instanciate_button() {
-    var dom = document.createElement('task-list');
+    var dom = document.createElement('preview-yap');
     $("#cover_content").append(dom);
 }
+
+function toggle_details(id) {
+    var p = document.querySelector('#' + id);
+    p.opened = p.opened ? 0 : 1;
+}
+
+$( document ).ready(function() {
+    $( "#player" ).hover(
+        function() {
+            var p = document.querySelector('#collapse_player');
+            p.opened = 1;
+        }, function() {
+            var p = document.querySelector('#collapse_player');
+            p.opened = 0;
+        }
+    );
+});
+
